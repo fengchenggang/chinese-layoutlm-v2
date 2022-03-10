c7639b3afa7df3fd99dc6422f1be05ef19b997f9
rain <303740673@qq.com>
2022-03-10 17:11:16 +0800

    ner模型的数据读取

--- examples/run_xfun_ser.py
+++ examples/run_xfun_ser.py
@@ -30,7 +30,7 @@ from transformers import (
 )
 from transformers.trainer_utils import get_last_checkpoint, is_main_process
 from transformers.utils import check_min_version
-
+from .data_process import generate_examples
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.5.0")
@@ -38,9 +38,19 @@ check_min_version("4.5.0")
 logger = logging.getLogger(__name__)
 
 
-def main():
-
+def ser():
     parser = HfArgumentParser((ModelArguments, XFUNDataTrainingArguments, TrainingArguments))
+    param = ['--model_name_or_path',
+             './output/test-ner-xfund/checkpoint-3900',
+             '--output_dir',
+             './output/test-ner-xfund',
+             '--do_predict',
+             '--lang',
+             'zh',
+             '--warmup_ratio',
+             '0.1',
+             '--fp16']
+    sys.argv.extend(param)
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
@@ -85,28 +95,14 @@ def main():
 
     # Set seed before initializing model.
     set_seed(training_args.seed)
-    datasets = load_dataset(
-        os.path.abspath(layoutlmft.data.datasets.xfun.__file__),
-        f"xfun.{data_args.lang}",
-        additional_langs=data_args.additional_langs,
-        keep_in_memory=True,
-    )
-    if training_args.do_train:
-        column_names = datasets["train"].column_names
-        features = datasets["train"].features
-    elif training_args.do_eval:
-        column_names = datasets["validation"].column_names
-        features = datasets["validation"].features
-    else:
-        column_names = datasets["test"].column_names
-        features = datasets["test"].features
-    text_column_name = "input_ids"
-    label_column_name = "labels"
 
-    remove_columns = column_names
+    # datasets = load_dataset(
+    #     os.path.abspath(layoutlmft.data.datasets.xfun.__file__),
+    #     f"xfun.{data_args.lang}",
+    #     additional_langs=data_args.additional_langs,
+    #     keep_in_memory=True,
+    # )
 
-    # In the event the labels are not a `Sequence[ClassLabel]`, we will need to go through the dataset to get the
-    # unique labels.
     def get_label_list(labels):
         unique_labels = set()
         for label in labels:
@@ -115,20 +111,9 @@ def main():
         label_list.sort()
         return label_list
 
-    if isinstance(features[label_column_name].feature, ClassLabel):
-        label_list = features[label_column_name].feature.names
-        # No need to convert the labels since they are already ints.
-        label_to_id = {i: i for i in range(len(label_list))}
-    else:
-        label_list = get_label_list(datasets["train"][label_column_name])
-        label_to_id = {l: i for i, l in enumerate(label_list)}
+    label_list = ['question', 'answer', 'other']
     num_labels = len(label_list)
 
-    # Load pretrained model and tokenizer
-    #
-    # Distributed training:
-    # The .from_pretrained methods guarantee that only one local process can concurrently
-    # download model & vocab.
     config = AutoConfig.from_pretrained(
         model_args.config_name if model_args.config_name else model_args.model_name_or_path,
         num_labels=num_labels,
@@ -164,27 +149,6 @@ def main():
     # Preprocessing the dataset
     # Padding strategy
     padding = "max_length" if data_args.pad_to_max_length else False
-
-    if training_args.do_train:
-        if "train" not in datasets:
-            raise ValueError("--do_train requires a train dataset")
-        train_dataset = datasets["train"]
-        if data_args.max_train_samples is not None:
-            train_dataset = train_dataset.select(range(data_args.max_train_samples))
-
-    if training_args.do_eval:
-        if "validation" not in datasets:
-            raise ValueError("--do_eval requires a validation dataset")
-        eval_dataset = datasets["validation"]
-        if data_args.max_val_samples is not None:
-            eval_dataset = eval_dataset.select(range(data_args.max_val_samples))
-
-    if training_args.do_predict:
-        if "test" not in datasets:
-            raise ValueError("--do_predict requires a test dataset")
-        test_dataset = datasets["test"]
-        if data_args.max_test_samples is not None:
-            test_dataset = test_dataset.select(range(data_args.max_test_samples))
 
     # Data collator
     data_collator = DataCollatorForKeyValueExtraction(
@@ -234,69 +198,33 @@ def main():
     trainer = XfunSerTrainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset if training_args.do_train else None,
-        eval_dataset=eval_dataset if training_args.do_eval else None,
+        train_dataset=None,
+        eval_dataset=None,
         tokenizer=tokenizer,
         data_collator=data_collator,
         compute_metrics=compute_metrics,
     )
 
-    # Training
-    if training_args.do_train:
-        checkpoint = last_checkpoint if last_checkpoint else None
-        train_result = trainer.train(resume_from_checkpoint=checkpoint)
-        metrics = train_result.metrics
-        trainer.save_model()  # Saves the tokenizer too for easy upload
 
-        max_train_samples = (
-            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
-        )
-        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
-
-        trainer.log_metrics("train", metrics)
-        trainer.save_metrics("train", metrics)
-        trainer.save_state()
-
-    # Evaluation
-    if training_args.do_eval:
-        logger.info("*** Evaluate ***")
-
-        metrics = trainer.evaluate()
-
-        max_val_samples = data_args.max_val_samples if data_args.max_val_samples is not None else len(eval_dataset)
-        metrics["eval_samples"] = min(max_val_samples, len(eval_dataset))
-
-        trainer.log_metrics("eval", metrics)
-        trainer.save_metrics("eval", metrics)
-
-    # Predict
-    if training_args.do_predict:
-        logger.info("*** Predict ***")
-
-        predictions, labels, metrics = trainer.predict(test_dataset)
-        predictions = np.argmax(predictions, axis=2)
-
-        # Remove ignored index (special tokens)
-        true_predictions = [
-            [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
-            for prediction, label in zip(predictions, labels)
-        ]
-
-        trainer.log_metrics("test", metrics)
-        trainer.save_metrics("test", metrics)
-
-        # Save predictions
-        output_test_predictions_file = os.path.join(training_args.output_dir, "test_predictions.txt")
-        if trainer.is_world_process_zero():
-            with open(output_test_predictions_file, "w") as writer:
-                for prediction in true_predictions:
-                    writer.write(" ".join(prediction) + "\n")
+    return trainer, label_list
 
 
-def _mp_fn(index):
-    # For xla_spawn (TPUs)
-    main()
+trainer, label_list = ser()
+
+
+def ner_infer():
+    test_dataset = generate_examples()
+    predictions, labels, metrics = trainer.predict(test_dataset)
+    predictions = np.argmax(predictions, axis=2)
+
+    # Remove ignored index (special tokens)
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    return true_predictions
 
 
 if __name__ == "__main__":
-    main()
+    ner_infer()
